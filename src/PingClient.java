7177ea8176ea74bf53fd6d7b97e984aa1fd17ab7
nitsan <nitsan@PushCart>
2012-12-04 00:02:31 +0000

    The ping/pong classes

---
+++ src/PingClient.java
@@ -0,0 +1,56 @@
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
+public class PingClient {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String host = args.length > 0 ? args[0] : "localhost";
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 12345;
+        System.out.println("Pinging "+host+":" + port);
+        SocketChannel sc = SocketChannel.open(new InetSocketAddress(host, port));
+
+        configure(sc);
+        ByteBuffer bb = ByteBuffer.allocateDirect(4096);
+        long times[] = new long[1000 * 1000];
+        for (int i = -10000; i < times.length; i++) {
+            long start = System.nanoTime();
+            bb.position(0);
+            bb.limit(32);
+            sc.write(bb);
+
+
+            bb.clear();
+            sc.read(bb);
+            long end = System.nanoTime();
+            long err = System.nanoTime() - end;
+            long time = end - start - err;
+            if (i >= 0)
+                times[i] = time;
+        }
+        close(sc);
+        Arrays.sort(times);
+        System.out.printf("RTT Latency for 1/50/99%%tile %.1f/%.1f/%.1f us%n",
+                times[times.length / 100] / 1e3,
+                times[times.length / 2] / 1e3,
+                times[times.length - times.length / 100 - 1] / 1e3
+        );
+    }
+
+    static void close(Closeable sc2) {
+        if (sc2 != null) try {
+            sc2.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+
+    static void configure(SocketChannel sc) throws SocketException {
+        sc.socket().setTcpNoDelay(true);
+    }
+}
---
+++ src/PingServer.java
@@ -0,0 +1,55 @@
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedByInterruptException;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class PingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String nic = args.length > 0 ? args[0] : "0.0.0.0";
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 12345;
+        System.out.println("Listening on interface : "+nic+":" + port);
+        final ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.socket().bind(new InetSocketAddress(nic, port));
+        SocketChannel sc2 = null;
+        try {
+            sc2 = ssc.accept();
+            configure(sc2);
+            sc2.configureBlocking(false);
+            close(ssc);
+            ByteBuffer bb2 = ByteBuffer.allocateDirect(4096);
+            while (!Thread.interrupted()) {
+                bb2.clear();
+                do {
+                    if(sc2.read(bb2) == -1)
+                        return;
+                } while (bb2.position() == 0);
+                bb2.flip();
+                sc2.write(bb2);
+            }
+        } catch (ClosedByInterruptException ignored) {
+        } catch (ClosedChannelException ignored) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(sc2);
+        }
+        System.in.read();
+    }
+    static void close(Closeable sc2) {
+        if (sc2 != null) try {
+            sc2.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+
+    static void configure(SocketChannel sc) throws SocketException {
+        sc.socket().setTcpNoDelay(true);
+    }
+}
