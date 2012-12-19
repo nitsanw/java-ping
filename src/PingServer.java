2504e61f7a9d7fcbf5215d8e5f54dee1c2c48b63
nitsan <nitsan@PushCart>
2012-12-19 23:07:30 +0000

    Added licence comments in headers

--- src/PingServer.java
+++ src/PingServer.java
@@ -1,11 +1,20 @@
-import java.io.Closeable;
+/*
+ * Heavily inspired by http://code.google.com/p/core-java-performance-examples/source/browse/trunk/src/test/java/com/google/code/java/core/socket/PingTest.java
+ * And therefore maintaining original licence:
+ * -------------------------------------------
+ * Copyright (c) 2011.  Peter Lawrey
+ *
+ * "THE BEER-WARE LICENSE" (Revision 128)
+ * As long as you retain this notice you can do whatever you want with this stuff.
+ * If we meet some day, and you think this stuff is worth it, you can buy me a beer in return
+ * There is no warranty.
+ * -------------------------------------------
+ * Further mutated by Nitsan Wakart.
+ */
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.channels.ClosedByInterruptException;
-import java.nio.channels.ClosedChannelException;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
