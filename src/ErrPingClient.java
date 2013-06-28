d10deacc89faefad31b25631fe4452abead6df51
nitsanw <nitsanw@yahoo.com>
2013-06-28 19:01:44 +0200

    Add measurement error client

---
+++ src/ErrPingClient.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class ErrPingClient extends AbstractPingClient {
+
+    public ErrPingClient(String[] args) throws IOException {
+        super(args);
+    }
+
+    @Override
+    void initChannel() throws IOException {
+    }
+
+    @Override
+    void cleanup() {
+    }
+
+    @Override
+    void ping(ByteBuffer bb) throws IOException {
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new ErrPingClient(args);
+    }
+}
